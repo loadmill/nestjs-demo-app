2ae5aba4beeda4424bebb42d5f3e28891292d477
yanirta <6667420+yanirta@users.noreply.github.com>
2022-08-15 18:19:14 +0300

    Adding loadmill be recorder

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { expressRecorder } = require('@loadmill/node-recorder');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(expressRecorder({
+    loadmillCode: process.env.LOADMILL_CODE,
+    notSecure: false,
+    cookieExpiration: 10 * 60 * 1000,
+    basePath: 'http://localhost:3001'
+  }));
+
   await app.listen(3001);
 }
 bootstrap();
